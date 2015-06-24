6f30fb918ac28168f832c8d25a4cb0d09e1add0f
tunnckoCore <mameto_100@mail.bg>
2015-06-24 21:25:06 +0300

    add tests

--- test.js
+++ test.js
@@ -10,8 +10,38 @@
 'use strict'
 
 var test = require('assertit')
-// var delValue = require('./index')
+var del = require('./index')
+var has = require('has-value')
 
 test('del-value:', function () {
-  // body
+  test('should return empty object if not object given', function (done) {
+    test.deepEqual(del(), {})
+    test.deepEqual(del(123456789), {})
+    test.deepEqual(del('strings'), {})
+    test.deepEqual(del([1, 2, 3]), {})
+    test.equal(!has(del()), !has({}))
+    done()
+  })
+  test('should return original object if `key` is not string or array', function (done) {
+    test.deepEqual(del({a: 'b'}, 22), {a: 'b'})
+    test.deepEqual(del({a: 'b'}, {}), {a: 'b'})
+    done()
+  })
+  test('should return original object if `key` not exist', function (done) {
+    test.deepEqual(del({a: 'b'}, 'foo'), {a: 'b'})
+    test.deepEqual(del({a: 'b'}, 'a.b.c'), {a: 'b'})
+    done()
+  })
+  test('should return modified object after `key` is deleted', function (done) {
+    test.deepEqual(del({a: {b: {c: 'ddd'}, e: 'ee'}, foo: 'bar'}, 'a'), {foo: 'bar'})
+    test.deepEqual(del({a: {b: {c: 'ddd'}, e: 'ee'}, foo: 'bar'}, 'a.b'), {a: {e: 'ee'}, foo: 'bar'})
+    done()
+  })
+  test('should support array of property paths', function (done) {
+    var actual = {a: {b: {c: 'ddd'}, e: 'ee'}, foo: 'bar', baz: {qux: 'dad', w: 'www'}}
+    var expected = {a: {e: 'ee'}, baz: {w: 'www'}}
+
+    test.deepEqual(del(actual, ['a.b', 'foo', 'baz.qux']), expected)
+    done()
+  })
 })
